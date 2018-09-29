bbdfc036eea2d5eafae5fa54775ad64cfbfa95e5
Henry <hyha@ucalgary.ca>
2018-09-28 19:53:32 -0600

    Added a new Activity" Test Activity

---
+++ app/src/main/java/dev/pool2go/TestActivity.java
@@ -0,0 +1,29 @@
+package dev.pool2go;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fabulous = (FloatingActionButton) findViewById(R.id.goToMaps);
+        fabulous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
